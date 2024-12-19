19809716788dda1c2615e438b1e93ede78b47966
Ivan Belyaev <vanobel159@gmail.com>
2024-12-20 01:01:38 +0300

    Drop unused code

--- src/users_generator.py
+++ src/users_generator.py
@@ -2,14 +2,10 @@ from datetime import datetime, timedelta
 
 from faker import Faker
 
-from src.di import di_container
-from src.models import User
-
 fake = Faker()
 
 def generate_users(size: int, start_date: datetime):
     raw_users = []
-    # session = di_container.db_session.instance
 
     for i in range(size):
         raw_user = {
@@ -19,8 +15,4 @@ def generate_users(size: int, start_date: datetime):
         }
         raw_users.append(raw_user)
 
-        # if i > 0 and i % percent_count == 0:
-    # session.bulk_insert_mappings(mapper=User, mappings=raw_users)
     return raw_users
-    # raw_users.clear()
-    # print(f"Progress: {i // percent_count}%")
